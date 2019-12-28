13807f90a9caf95349397a7cb0c00aff20aada90
katia <katia@localhost>
2019-12-28 01:52:43 +0100

    :(

--- synth/src/application/Main.java
+++ synth/src/application/Main.java
@@ -1,25 +1,80 @@
-package application;
+ package application;
 	
+import java.io.BufferedWriter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
+import model.AudioThread;
+import model.Synthesizer;
+import views.MainViewController;
+import views.ViewController;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
 
 public class Main extends Application {
+	private Scene scene;
+	private Stage primaryStage;
+	
+	private boolean shouldGenerate;
+	private int wavePos;
+	
+	private final AudioThread audioThread = new AudioThread(() -> {
+		if(!shouldGenerate) {
+			return null;
+		}
+		short s [] = new short[AudioThread.BUFFER_SIZE];
+		for(int i = 0; i < AudioThread.BUFFER_SIZE; i++) {
+			s[i] = (short) (Short.MAX_VALUE * Math.sin(2 * Math.PI * 200) / Synthesizer.AudioInfo.SAMPLE_RATE * wavePos++) ;
+		}
+		return s; //return buffer
+	});
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			this.primaryStage = primaryStage;
+			
+			ViewController<Main> controller;
+			controller = new MainViewController(this);
+			Pane root = controller.getRootView();
+			Scene scene = new Scene(root,800,600);
+			
+			scene.setOnKeyPressed((KeyEvent event) -> {
+	            System.out.println("Key Pressed");
+	            if(!audioThread.isRunning()) {
+	            	System.out.println("-----");
+	            	shouldGenerate = true;
+	            	audioThread.triggerPlayback();
+	            }
+	        });
+			
+			scene.setOnKeyReleased((KeyEvent event) -> {
+            	shouldGenerate = false;
+	        });
+			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
+	@Override
+	public void init() {
+	}
+	
+	@Override
+	public void stop() {
+		audioThread.stop();
+	}
 	public static void main(String[] args) {
 		launch(args);
 	}
